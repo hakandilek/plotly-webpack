e15d59b5645cb61170dd488d0da33f33fe39b4ec
Hakan Dilek <hakandilek@gmail.com>
2018-06-15 16:04:33 +0200

    try a 3d plot

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 var Plotly = require('plotly.js');
 
-Plotly.plot("gd", [{ x: [0, 1], y: [0, 1] }]);
+// Generating random data..
+a=[]; b=[]; c=[];
+for(i=0;i<50;i++)
+  {
+    var a_ = Math.random(); 
+    a.push(a_);
+    
+    var b_ = Math.random(); 
+    b.push(b_);
+    
+    var c_ = Math.random(); 
+    c.push(c_);
+  }
+// Plotting the mesh
+var data=[
+    {
+      opacity:0.8,
+      color:'rgb(300,100,200)',
+      type: 'mesh3d',
+      x: a,
+      y: b,
+      z: c,
+    }
+];
+
+Plotly.plot("gd", data);
